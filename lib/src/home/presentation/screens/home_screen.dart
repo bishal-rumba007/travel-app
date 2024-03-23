06c2d672204159750e4a558235d26145dc77131e
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-03-23 23:42:40 +0545

    create initial UI for home screen

--- lib/src/home/presentation/screens/home_screen.dart
+++ lib/src/home/presentation/screens/home_screen.dart
@@ -23,57 +23,97 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         toolbarHeight: 0,
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 16.h,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 16.h,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "GoGo",
+                      style: theme.textTheme.headlineLarge?.copyWith(
+                          fontSize: 34.sp,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/map.svg")
+                  ],
+                ),
+                SizedBox(height: 8.h,),
                 Text(
-                  "GoGo",
-                  style: theme.textTheme.headlineLarge?.copyWith(
-                    fontSize: 34.sp,
-                    fontWeight: FontWeight.w500
+                  'Choose Another',
+                  style: theme.textTheme.labelSmall?.copyWith(
+                    color: AppColor.textColor.withOpacity(0.5),
                   ),
                 ),
-                SvgPicture.asset("assets/icons/map.svg")
+                SizedBox(height: 24.h,),
+                GestureDetector(
+                  onTap: () {},
+                  child: TextField(
+                    enabled: false,
+                    style: theme.textTheme.bodyMedium?.copyWith(
+                        fontSize: 15.sp
+                    ),
+                    decoration: InputDecoration(
+                      hintText: "Enter name or category",
+                      suffixIcon: Icon(CupertinoIcons.search, size: 20.h,),
+                    ),
+                  ),
+                ),
               ],
             ),
-            SizedBox(height: 8.h,),
-            Text(
-              'Choose Another',
-              style: theme.textTheme.labelSmall?.copyWith(
-                color: AppColor.textColor.withOpacity(0.5),
-              ),
-            ),
-            SizedBox(height: 24.h,),
-            GestureDetector(
-              onTap: () {},
-              child: TextField(
-                enabled: false,
-                style: theme.textTheme.bodyMedium?.copyWith(
-                  fontSize: 15.sp
-                ),
-                decoration: InputDecoration(
-                  hintText: "Enter name or category",
-                  suffixIcon: Icon(CupertinoIcons.search, size: 20.h,),
-                ),
-              ),
-            ),
-            SizedBox(height: 28.h,),
-            Text(
+          ),
+          SizedBox(height: 28.h,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Text(
               "Category",
               style: theme.textTheme.titleMedium?.copyWith(
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(height: 16.h,),
-
-          ],
-        ),
+          ),
+          SizedBox(height: 16.h,),
+          SizedBox(
+            height: 52.h,
+            child: ListView.separated(
+              itemCount: 3,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: index == 0 ? EdgeInsets.only(left: 24.w) : null,
+                  padding: EdgeInsets.only(left: 8.w, top: 8.h, right: 16.w, bottom: 8.h),
+                  height: 52.h,
+                  width: 164.w,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF8F8F8),
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 36.h,
+                        width: 36.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.r),
+                          color: AppColor.beige,
+                        ),
+                        child: const Icon(Icons.star_rounded, color: Colors.white,),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(width: 12.w,),
+            ),
+          ),
+        ],
       )
     );
   }
