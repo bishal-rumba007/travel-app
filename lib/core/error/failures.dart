4ba19337529fb01e7da8a9e1e3174befdffcaa6c
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-03-25 22:42:36 +0545

    Add custom exceptions and failures

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final List properties;
+
+  const Failure([this.properties = const <dynamic>[]]);
+
+  @override
+  List<Object?> get props => properties;
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure() : super();
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure() : super();
+}
